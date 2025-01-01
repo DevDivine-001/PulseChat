d1d1cb557398828d608b8b083a32035b15035361
Oscdivin <divineuwandu84@Gmail.com>
2025-01-01 19:29:05 +0100

    divine update the code base just now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,46 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
+import SignUp from "./pages/SignUp"
+import Homepage from "./pages/Homepage"
+import LogIn from "./pages/LogIn"
+import Settings from "./pages/Settings"
+import Profile from "./pages/Profile"
+import { AuthStore } from "./store/Auth.store"
+import { useEffect } from "react"
+import { Loader } from "lucide-react"
+import LandingPages from "./pages/LandingPages"
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = AuthStore()
+
+  useEffect(() => {
+    checkAuth()
+
+  }, [checkAuth])
+
+  console.log({ authUser })
+
+
+  if (isCheckingAuth && !authUser) return (<div>
+    <div className="justify-center items-center flex h-screen bg-black">
+      <Loader className="size-10 animate-spin" />
+    </div>
+
+  </div>)
+
   return (
-    <div className="text-red-600">
-      App
+    <div className="text-white">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<LandingPages />} />
+        <Route path="/homepage" element={authUser ? <Homepage /> : <Navigate to="/signup" />} />
+        <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to="/homepage" />} />
+        <Route path="/login" element={!authUser ? <LogIn /> : <Navigate to="/homepage" />} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login" />} />
+        <Route path="/settings" element={<Settings />} />
+
+      </Routes>
+      {/* <Footer /> */}
     </div>
   )
 }
---
+++ frontend/src/pages/Hero.jsx
@@ -0,0 +1,20 @@
+
+const Hero = () => {
+  return (
+    <div className="w-full bg-[blue] flex items-center justify-center h-[100vh]">
+      <div className="bg-[green] flex  items-center justify-between  mx-auto h-[90vh] container flex-wrap gap-2 px-4">
+        <div className="w-[600px] bg-black flex justify-center items-center h-[70vh] max-sm:h-[40vh] max-lg:h-[40vh] md:w-full lg:w-[600px] max-sm:w-full">
+
+        </div>
+
+        <div className="w-[600px] bg-[red] flex justify-center items-center h-[70vh] max-sm:h-[37vh] max-lg:h-[36vh] md:w-full lg:w-[600px]">
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Hero
