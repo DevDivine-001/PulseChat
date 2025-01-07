c4be48a9f69d9bea4e7351b405ce4b99cf13e296
Oscdivin <divineuwandu84@Gmail.com>
2025-01-07 14:48:12 +0100

    GhatHeader file is been creater now

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react"
+import { useChatState } from "../store/ChatStore"
+import MessageInput from "./MessageInput"
+import ChatHeader from "./ChatHeader"
+import { Loader2 } from "lucide-react"
 
 const ChatBox = () => {
+    const { messages, getMessages, isMessagesLoading, selectedUser } = useChatState()
+
+
+    useEffect(() => {
+        getMessages(selectedUser._id)
+
+    }, [selectedUser._id, getMessages])
+
+    if (isMessagesLoading) return <div className="flex justify-center items-center w-full h-[80vh]"> <Loader2 className="animate-spin size-7" />Loading ...!</div>
     return (
-        <div>ChatBox</div>
+        <div className="flex-1 flex flex-col overflow-auto">
+            <ChatHeader />
+
+            <p>Messages...</p>
+
+            <MessageInput />
+        </div>
     )
 }
 
