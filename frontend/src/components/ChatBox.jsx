ca9937ff8b7598cc7635db8cbbeafd84eacd4093
Oscdivin <divineuwandu84@Gmail.com>
2025-01-07 15:12:47 +0100

    message chat part is amost

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -3,6 +3,7 @@ import { useChatState } from "../store/ChatStore"
 import MessageInput from "./MessageInput"
 import ChatHeader from "./ChatHeader"
 import { Loader2 } from "lucide-react"
+import MessageSkeletons from "./Skeletons/MessageSkeletons"
 
 const ChatBox = () => {
     const { messages, getMessages, isMessagesLoading, selectedUser } = useChatState()
@@ -13,7 +14,16 @@ const ChatBox = () => {
 
     }, [selectedUser._id, getMessages])
 
-    if (isMessagesLoading) return <div className="flex justify-center items-center w-full h-[80vh]"> <Loader2 className="animate-spin size-7" />Loading ...!</div>
+    if (!isMessagesLoading) return (
+        <div className="flex  flex-col flex-1 overflow-auto">
+            <div className="flex justify-center items-center">
+                <Loader2 className="animate-spin size-7 absolute  mt-64 flex justify-center items-center" />
+            </div>
+            <ChatHeader />
+            <MessageSkeletons />
+            <MessageInput />
+        </div>
+    )
     return (
         <div className="flex-1 flex flex-col overflow-auto">
             <ChatHeader />
